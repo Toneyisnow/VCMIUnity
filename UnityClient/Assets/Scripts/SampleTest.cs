4cf67fe93bdc4fbbc82f6600fbbc3c900101f5f2
Toney Sui <charlie_5899@hotmail.com>
2019-11-27 16:50:57 -0800

    Investigating the Materials.

--- UnityClient/Assets/Scripts/SampleTest.cs
+++ UnityClient/Assets/Scripts/SampleTest.cs
@@ -45,7 +45,7 @@ public class SampleTest : MonoBehaviour
 
         // LoadAnimation();
 
-        LoadTerrain();
+        TestTextureRenderer();
 
         sceneLoaded = true;
         frameCount = 0;
@@ -117,6 +117,95 @@ public class SampleTest : MonoBehaviour
 
         AnimatedObject animated = obj.GetComponent<AnimatedObject>();
         animated.Initialize(animation);
+    }
+
+    void TestTextureRenderer()
+    {
+        Engine engine = Engine.GetInstance();
+
+        engine.LoadArchiveFile(GetGameDataFilePath("H3ab_bmp.lod"));
+        engine.LoadArchiveFile(GetGameDataFilePath("H3ab_spr.lod"));
+        engine.LoadArchiveFile(GetGameDataFilePath("H3bitmap.lod"));
+        engine.LoadArchiveFile(GetGameDataFilePath("H3sprite.lod"));
+
+        H3Campaign campaign = engine.RetrieveCampaign("ab.h3c");
+
+        H3Map map = H3CampaignLoader.LoadScenarioMap(campaign, 0);
+
+        Dictionary<Texture2D, List<Vector2>> textureDict = new Dictionary<Texture2D, List<Vector2>>();
+        Dictionary<string, Sprite> spriteDictionary = new Dictionary<string, Sprite>();
+        int levelCount = (map.Header.IsTwoLevel ? 2 : 1);
+        for (int level = 0; level < 1; level++)
+        {
+            for (int xx = 0; xx < map.Header.Width; xx++)
+            {
+                for (int yy = 0; yy < map.Header.Height; yy++)
+                {
+                    TerrainTile tile = map.TerrainTiles[level, xx, yy];
+
+                    ImageData terrainImage = engine.RetrieveTerrainImage(tile.TerrainType, tile.TerrainView);
+                    Texture2D terrainTexture = TextureStorage.GetInstance().LoadTerrainTexture(tile.TerrainType, tile.TerrainView, tile.TerrainRotation, terrainImage.GetPNGData(tile.TerrainRotation));
+
+                    List<Vector2> positions = null;
+                    if (textureDict.ContainsKey(terrainTexture))
+                    {
+                        positions = textureDict[terrainTexture];
+                    }
+                    else
+                    {
+                        positions = new List<Vector2>();
+                        textureDict[terrainTexture] = positions;
+                    }
+
+                    positions.Add(new Vector2(xx, yy));
+                }
+            }
+
+            Shader shader = Shader.Find("Sprites/Default");
+            foreach (Texture2D texture in textureDict.Keys)
+            {
+                MaterialPropertyBlock block = new MaterialPropertyBlock();
+                block.SetTexture("tl", texture);
+
+                Material material = new Material(shader);
+                //// material.enableInstancing = true;
+                material.mainTexture = texture;
+                
+                List<Vector2> positions = textureDict[texture];
+                foreach (Vector2 pos in positions)
+                {
+                    string spriteKey = string.Format(@"{0}-{1}-{2}", "tl", pos.x, pos.y);
+                    //// Sprite sprite = null;
+
+                    /*
+                    if (spriteDictionary.ContainsKey(spriteKey))
+                    {
+                        sprite = spriteDictionary[spriteKey];
+                    }
+                    else
+                    {
+                        sprite = CreateSpriteFromTexture(texture);
+                        spriteDictionary[spriteKey] = sprite;
+                    }
+                    */
+
+                    Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+                    GameObject g1 = new GameObject();
+                    g1.transform.position = GetMapPositionInPixel((int)pos.x, (int)pos.y, -1);
+                    g1.transform.parent = transform;
+                    
+                    SpriteRenderer renderer = g1.AddComponent<SpriteRenderer>();
+                    renderer.drawMode = SpriteDrawMode.Simple;
+                    
+                    renderer.sharedMaterial = material;
+                    renderer.sprite = sprite;
+
+                    //// renderer.sprite = sprite;
+
+                    //// renderer.sharedMaterial.SetTexture(spriteKey, terrainTexture);
+                }
+            }
+        }
     }
 
     void LoadTerrain()
