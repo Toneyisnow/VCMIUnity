63d2f8880cf9bf6932e83f087898064adc6980f8
Toney Sui <charlie_5899@hotmail.com>
2019-12-20 07:33:04 -0800

    Update touch method.

--- UnityClient/Assets/Scripts/MapCamera.cs
+++ UnityClient/Assets/Scripts/MapCamera.cs
@@ -7,6 +7,8 @@ public class MapCamera : MonoBehaviour
     private Vector3 anchorPosition = Vector3.zero;
     private Vector3 lastPosition = Vector3.zero;
 
+    private float anchorDistance = 0;
+    private float lastScale = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -17,26 +19,63 @@ public class MapCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int touchCount = Input.touchCount;
+        var cameraCom = this.GetComponent<Camera>();
+
         if (Input.GetMouseButtonDown(0))
         {
-            anchorPosition = Input.mousePosition;
-            lastPosition = this.transform.position;
-            return;
+            if (touchCount == 1)
+            {
+                // Click on screen
+                anchorPosition = Input.mousePosition;
+                lastPosition = this.transform.position;
+                return;
+            }
+            else if (touchCount == 2)
+            {
+                // Two touch on screen
+                Touch touch1 = Input.touches[0];
+                Touch touch2 = Input.touches[1];
+
+                anchorDistance = Mathf.Abs(touch1.position.x - touch2.position.x) + Mathf.Abs(touch1.position.y - touch2.position.y);
+
+                lastScale = cameraCom.orthographicSize;
+                return;
+            }
         }
 
         if (Input.GetMouseButton(0))
         {
-            // Dragging
-            float deltaX = (Input.mousePosition.x - anchorPosition.x) / 100;
-            float deltaY = (Input.mousePosition.y - anchorPosition.y) / 100;
+            if (touchCount == 1)
+            {
+                // Dragging
+                float deltaX = (Input.mousePosition.x - anchorPosition.x) / 100 * cameraCom.orthographicSize;
+                float deltaY = (Input.mousePosition.y - anchorPosition.y) / 100 * cameraCom.orthographicSize;
 
-            Vector3 newPosition = new Vector3(lastPosition.x - deltaX, lastPosition.y - deltaY, lastPosition.z);
-            this.transform.position = newPosition;
-        }
+                Vector3 newPosition = new Vector3(lastPosition.x - deltaX, lastPosition.y - deltaY, lastPosition.z);
+                this.transform.position = newPosition;
+            }
+            else if (touchCount == 2)
+            {
+                if (anchorDistance > 0)
+                {
+                    Touch touch1 = Input.touches[0];
+                    Touch touch2 = Input.touches[1];
 
-        if (Input.touchCount > 0)
-        {
-            int h = 1;
+                    float currentDistance = Mathf.Abs(touch1.position.x - touch2.position.x) + Mathf.Abs(touch1.position.y - touch2.position.y);
+
+                    
+                    var currentScale = (anchorDistance / currentDistance) * lastScale;
+                    currentScale = Mathf.Max(currentScale, 1.0f);
+                    currentScale = Mathf.Min(currentScale, 6.0f);
+
+                    cameraCom.orthographicSize = currentScale;
+
+                }
+
+                return;
+            }
+
         }
 
     }
