d008eb4bb0501fb2e37b81e6eca0bbf5709e0678
Toney Sui <charlie_5899@hotmail.com>
2020-01-05 02:08:02 -0800

    Re-fix the MapCamera.

--- UnityClient/Assets/Scripts/MapCamera.cs
+++ UnityClient/Assets/Scripts/MapCamera.cs
@@ -19,6 +19,81 @@ public class MapCamera : MonoBehaviour
 
     private void LateUpdate()
     {
+        if (Input.touchSupported)
+        {
+            HandleTouches();
+        }
+        else
+        {
+            HandleMouse();
+        }
+    }
+
+    private void HandleTouches()
+    {
+        int touchCount = Input.touchCount;
+        if (touchCount == 1)
+        {
+            Touch touch1 = Input.touches[0];
+            if (touch1.phase == TouchPhase.Began)
+            {
+                Origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            else if (touch1.phase == TouchPhase.Moved)
+            {
+                Difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Camera.main.transform.position;
+
+                if (Difference != Vector3.zero)
+                {
+                    //// print("dragging.");
+                    Camera.main.transform.position = Origin - Difference;
+                }
+            }
+        }
+        else if(touchCount == 2)
+        {
+            Touch touch1 = Input.touches[0];
+            Touch touch2 = Input.touches[1];
+
+            if (touch1.phase == TouchPhase.Began || touch2.phase == TouchPhase.Began)
+            {
+                anchorDistance = Mathf.Abs(touch1.position.x - touch2.position.x) + Mathf.Abs(touch1.position.y - touch2.position.y);
+                lastScale = Camera.main.orthographicSize;
+            }
+            else if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved)
+            {
+                if (anchorDistance > 0)
+                {
+                    float currentDistance = Mathf.Abs(touch1.position.x - touch2.position.x) + Mathf.Abs(touch1.position.y - touch2.position.y);
+                    var currentScale = (anchorDistance / currentDistance) * lastScale;
+                    currentScale = Mathf.Max(currentScale, 1.0f);
+                    currentScale = Mathf.Min(currentScale, 6.0f);
+
+                    Camera.main.orthographicSize = currentScale;
+                }
+            }
+        }
+    }
+
+    private void HandleMouse()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Camera.main.transform.position;
+
+            if (Difference != Vector3.zero)
+            {
+                Camera.main.transform.position = Origin - Difference;
+            }
+        }
+    }
+
+    private void LateUpdate2()
+    {
         int touchCount = Input.touchCount;
         if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
         {
